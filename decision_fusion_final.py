4f149d429184523b6f3dfcadfcbf26e733c30747
denizyennerr <dyener@tlue.ee>
2026-03-16 15:27:45 +0200

    Project updated

--- decision_fusion_final.py
+++ decision_fusion_final.py
@@ -23,13 +23,13 @@ datasets_info = {
     '10s': {'data': 'master_dataset_10s.npz', 'models_dir': 'saved_outputs/20260309-155522_10s/models'},
 }
 
-output_csv_name = "all_models_performance_comparison_1s_ref_0.5tresh.csv"
+output_csv_name = "all_models_performance_comparison_1s_ref_modelweight.csv"
 
 # Weights based on Eval script insights
-model_weights = {'0.5s': 1.0, '1s': 3.0, '2s': 1.0, '4s': 1.0, '5s': 0.0, '10s': 0.0}
+model_weights = {'0.5s': 1.0, '1s': 3.5, '2s': 1.0, '4s': 0.5, '5s': 0.0, '10s': 0.0}
 
 # FIXED A PRIORI CLINICAL THRESHOLD
-FIXED_THRESHOLD = 0.5
+FIXED_THRESHOLD = 0.3
 
 
 # =============================================================================
